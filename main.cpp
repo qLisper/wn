10f19aa664446ac451d6725339dfa12c2be2bf5b
qLisper <arseniymusorin@gmail.com>
2025-12-02 10:00:28 +0300

    init

--- main.cpp
+++ main.cpp
@@ -1,6 +1,41 @@
 #include <iostream>
-using namespace std;
+#include <stdexcept>
+namespace top {
+  struct p_t {
+    int x, y;
+  };
+  struct f_t {
+    p_t a, p_t b;
+  };
+  bool operator==(p_t a, p_t b);
+  bool operator!=(p_t a, p_t b);
+  struct IDraw {
+    virtual p_t begin() const = 0;
+    virtual p_t next(p_t prev) const = 0;
+    virtual ~IDraw() = default;
+  };
+  struct Dot: IDraw {
+    p_t begin() const override;
+    p_t next(p_t prev) const override;
+    p_t d;
+  };
 int main(){
-cout << "Словно сто добровольных лет одиночества" << endl;
-return 0;
+  using namespace top;
+  p_t a{1, 1}, b{0,1};
+  std::cout << (a == b) << "\n";
+}
+top::p_t top::Dot::begin() const {
+  return d;
+}
+top::p_t top::Dot::next(p_t prev) const {
+  if (prev != d) {
+    throw std::logic_error("bad prev"); 
+  }
+  return d;
+}
+bool top::operator==(p_t a, p_t b) {
+  return a.x == b.x && a.y == b.y;
+}
+bool top::operator!=(p_t a, p_t b) {
+  return !(a == b);
 }
